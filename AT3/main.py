12892e57c70630b3a7162e74b8e1c461dc2e1724
Augusto H. V. Guerner <augusto.h.v.guerner@gmail.com>
2024-07-06 11:05:25 -0300

    improving code

--- AT3/main.py
+++ AT3/main.py
@@ -41,6 +41,76 @@ def calculate_xi_dot_pi(xis, pis):
 def calculate_idk(xis, pis, mean):
     return [(xi - mean)**2 * pi for xi, pi in zip(xis, pis)]
 
+def calculate_descriptive_model(data):
+    n = len(data)
+    sturges = sturges_formula(n)
+    root = square_root(n)
+    number_of_class = sturges
+
+    minimum = data.min()
+    maximum = data.max()
+    r = maximum - minimum
+    c =  round(r / number_of_class, 4)
+    mean = data.mean()
+    median = data.median()
+    variance_value = data.var()
+    std_deviation_value = data.std()
+
+    class_intervals = get_class_interval(minimum, round(number_of_class), c)
+    xis = calculate_xis(class_intervals)
+
+    freqs = calculate_freq(data, class_intervals)
+    
+    pis = calculate_pis(freqs, n)
+    xis_dot_pis = calculate_xi_dot_pi(xis, pis)
+    idk = calculate_idk(xis, pis, mean)
+
+    print("MODELO DESCRITIVO " + "="*220)
+    print()
+    print(f"Número de dados")
+    print(f"Número de classes sturgues {sturges}")
+    print(f"Número de classes raiz {root}")
+    print(f"Número de classes escolhido {number_of_class}")
+    print()
+    print(f"Valor mínimo {minimum}")
+    print(f"Valor máximo {maximum}")
+    print(f"Valor de R {r}")
+    print(f"Valor de C {c}")
+    print()
+    print(f"Média {mean}")
+    print(f"Mediana {median}")
+    print(f"Variância {variance_value}")
+    print(f"Desvio Padrão {std_deviation_value}")
+    print()
+    print(f"Intervalos da classes {class_intervals}")
+    print(f"Ponto médio dos intervalos {xis}")
+    print(f"Frequências dos dados {freqs}")
+    print(f"Soma das frequências {sum(freqs)}")
+    print(f"Probabilidade de cada intervalo {pis}")
+    print(f"Probabilidade x Ponto médio de cada intervalo {xis_dot_pis}")
+    print(f"Não faço ideia do que é {idk}")
+    print("="*238)
+    print()
+
+    return {
+        "numero de dados": n,
+        "numero de classes": number_of_class,
+        "max": maximum,
+        "min": minimum,
+        "range": r,
+        "c": c,
+        "media": mean,
+        "mediana": median,
+        "variancia": variance_value,
+        "desvio padrao": std_deviation_value,
+        "intervalos": class_intervals,
+        "xis": xis,
+        "freqs": freqs,
+        "pis": pis,
+        "xis dot pis": xis_dot_pis,
+        "idk": idk
+    }
+
 def calculate_zs(intervals, mean, std):
     return zip(*[((a - mean) / std, (b - mean) / std) for a, b in intervals])
 
@@ -104,6 +174,48 @@ def adjust(intervals, freqs, es, error=0.05):
 
     return adjust_intervals, adjust_freqs, adjust_es
 
+def chi_square(n, number_of_class, class_intervals, mean, std_deviation_value, freqs):
+    df = number_of_class - 2 - 1
+    chi2_critical = stats.chi2.ppf(0.95, df)
+
+    z1s, z2s = calculate_zs(class_intervals, mean, std_deviation_value)
+    p_z1s_z2s = calculate_probability(z1s, z2s)
+    es = calculate_es(p_z1s_z2s, n)
+    adjust_intervals, adjust_freqs, adjust_es = adjust(class_intervals, freqs, es)
+    qui_squares = calculate_qui_squares(adjust_freqs, adjust_es)
+
+    print("TESTE DE ADERÊNCIA " + "="*219)
+    print()
+    print(f"Valores de z1 {z1s}")
+    print(f"Valores de z2 {z2s}")
+    print(f"P(z1 < Z < z2) {p_z1s_z2s}")
+    print(f"Soma de probabilidade {sum(p_z1s_z2s)}")
+    print(f"Probabilidades esperadas {es}")
+    print(f"Soma das probabilidades esperadas {sum(es)}")
+    print()
+
+    print("AJUSTE TESTE DE ADERÊNCIA")
+    print(f"Intervalo ajustado {adjust_intervals}")
+    print(f"Frequência ajustada {adjust_freqs}")
+    print(f"Probabilidade esperada ajustada {adjust_es}")
+    print(f"Qui squares {qui_squares}")
+    print(f"Qui square {sum(qui_squares)}")
+    print(f"Qui square critico {chi2_critical}")
+    print("="*237)
+    print()
+
+    chi2, p = stats.chisquare(adjust_freqs, f_exp=adjust_es)
+    chi2_critical_log = stats.chi2.ppf(0.95, df)
+    print(chi2)
+    print(p)
+    print(chi2_critical_log)
+
+    if chi2 > chi2_critical_log:
+        print("Rejeitamos H0: Os dados não seguem uma distribuição normal.")
+    else:
+        print("Não rejeitamos H0: Os dados seguem uma distribuição normal.")
+
+    return qui_squares, chi2_critical
 
 def main():
 
@@ -137,88 +249,20 @@ def main():
     print("Tabela só com os dados do Amstel Lager")
     print(amstel_lager)
     print()
-    
-    print("Calculando o número de classes para Amstel Lager")
-    print()
-
-    n = len(amstel_lager)
-    sturges = sturges_formula(n)
-    root = square_root(n)
-    number_of_class = sturges
-    df = number_of_class - 2 - 1
-
-    print(f"Número de dados {n}")
-    print(f"Número de classes Sturges {sturges}")
-    print(f"Número de classes Raiz {root}")
-    print("Foi escolhido sturges")
-    print()
 
     col = "C03 - VIDRO 600ML RET"
-    prices = np.log(amstel_lager[col])
-
-    minimum = prices.min()
-    maximum = prices.max()
-    r = maximum - minimum
-    c =  round(r / number_of_class, 4)
-    mean = prices.mean()
-    median = prices.median()
-    variance_value = prices.var()
-    std_deviation_value = prices.std()
-
-    class_intervals = get_class_interval(minimum, round(number_of_class), c)
-    xis = calculate_xis(class_intervals)
-
-    freqs = calculate_freq(prices, class_intervals)
+    prices = amstel_lager[col]
     
-    pis = calculate_pis(freqs, n)
-    xis_dot_pis = calculate_xi_dot_pi(xis, pis)
-    idk = calculate_idk(xis, pis, mean)
+    model = calculate_descriptive_model(prices)
+    n = model["numero de dados"]
+    number_of_class = model["numero de classes"]
+    class_intervals = model["intervalos"]
+    freqs = model["freqs"]
+    mean = model["media"]
+    std_deviation_value = model["desvio padrao"]
 
-    z1s, z2s = calculate_zs(class_intervals, mean, std_deviation_value)
-    p_z1s_z2s = calculate_probability(z1s, z2s)
-    es = calculate_es(p_z1s_z2s, n)
-    adjust_intervals, adjust_freqs, adjust_es = adjust(class_intervals, freqs, es)
-    qui_squares = calculate_qui_squares(adjust_freqs, adjust_es)
+    chi_square_value, chi_critical = chi_square(n, number_of_class, class_intervals, mean, std_deviation_value, freqs)
 
-    print("MODELO DESCRITIVO")
-    print(f"Valor mínimo {minimum}")
-    print(f"Valor máximo {maximum}")
-    print(f"Valor de R {r}")
-    print(f"Valor de C {c}")
-    print(f"Média {mean}")
-    print(f"Mediana {median}")
-    print(f"Variância {variance_value}")
-    print(f"Desvio Padrão {std_deviation_value}")
-    print(f"Intervalos da classes {class_intervals}")
-    print(f"Ponto médio dos intervalos {xis}")
-    print(f"Frequências dos dados {freqs}")
-    print(f"Soma das frequências {sum(freqs)}")
-    print(f"Probabilidade de cada intervalo {pis}")
-    print(f"Probabilidade x Ponto médio de cada intervalo {xis_dot_pis}")
-    print(f"Não faço ideia do que é {idk}")
-    print()
-
-    print("TESTE DE ADERÊNCIA")
-    print(f"Valores de z1 {z1s}")
-    print(f"Valores de z2 {z2s}")
-    print(f"P(z1 < Z < z2) {p_z1s_z2s}")
-    print(f"Soma de probabilidade {sum(p_z1s_z2s)}")
-    print(f"Probabilidades esperadas {es}")
-    print(f"Soma das probabilidades esperadas {sum(es)}")
-    print()
-
-    print("AJUSTE TESTE DE ADERÊNCIA")
-    print(f"Intervalo ajustado {adjust_intervals}")
-    print(f"Frequência ajustada {adjust_freqs}")
-    print(f"Probabilidade esperada ajustada {adjust_es}")
-    print(f"Qui squares {qui_squares}")
-    print(f"Qui square {sum(qui_squares)}")
-    print()
-
-    # print("Criando a tabela")
-    # print()
-
-    # print("Histograma dos dados")
 
     # # Construindo os histogramas
     # # Colocar os dois histogramas juntos com ...subplots(1, 2, ...
@@ -232,36 +276,6 @@ def main():
 
     # # plt.tight_layout()
     # plt.show()
-
-    chi2, p = stats.chisquare(adjust_freqs, f_exp=es)
-    chi2_critical_log = stats.chi2.ppf(0.95, df)
-    print(chi2)
-    print(p)
-    print(chi2_critical_log)
-
-    if chi2 > chi2_critical_log:
-        print("Rejeitamos H0: Os dados não seguem uma distribuição normal.")
-    else:
-        print("Não rejeitamos H0: Os dados seguem uma distribuição normal.")
-
-    # import numpy as np
-    # import scipy.stats as stats
-
-    # # Suponha que você tenha um conjunto de dados chamado data
-    # data = np.random.lognormal(mean=0, sigma=1, size=100)  # Exemplo de dados log-normal
-
-    # # Aplique a transformação logarítmica aos dados
-    # log_data = np.log(prices)
-
-    # # Teste de Shapiro-Wilk para verificar a normalidade dos dados transformados
-    # shapiro_test = stats.shapiro(log_data)
-    # print(f"Shapiro-Wilk Test: Estatística = {shapiro_test.statistic}, p-valor = {shapiro_test.pvalue}")
-
-    # # Teste de Kolmogorov-Smirnov para a distribuição log-normal
-    # mean, sigma = np.mean(log_data), np.std(log_data, ddof=1)
-    # ks_test = stats.kstest(prices, 'lognorm', args=(sigma, 0, np.exp(mean)))
-    # print(f"Kolmogorov-Smirnov Test: Estatística = {ks_test.statistic}, p-valor = {ks_test.pvalue}")
-
 
 
 if __name__ == "__main__":
